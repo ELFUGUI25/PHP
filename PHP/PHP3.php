e42254057071f82d18b5656472c794235ee92a8d
ELFUGUI25 <secocuellard@gmail.com>
2024-12-05 14:03:06 +0100

    PHP

--- PHP/PHP3.php
+++ PHP/PHP3.php
@@ -11,14 +11,14 @@
     <strong>¿Que significa php?</strong><br>
     <echo>PHP se ejecuta en el servidor y nos devuelve la web en HTML. Además, es un lenguaje libre y muy popular con el
         cual podemos crear cualquier web que deseemos, desde una web de información hasta una pasarela de pagos o una
-        tienda on-line. </echo>
+        tienda on-line. </echo> /* esto es un tipo de comentario */.
 
     <br><br><strong>¿Para que sirve echo?</strong><br>
     <echo>Para escribir texto en pantalla, al igual que un "p" en html. </echo><br><br>
 
     <strong>¿Que ocurre cuando ejecutas un archivo en el servidor?</strong><br>
     <echo>Depende de algunos factores, pero puede generar una interpretacion de archivo, permisos de ejecucion, etc
-    </echo>
+    </echo> //.Esto es otro tipo de comentario
 
 </body>
 
