51de9cc89f05a88254b097e859ffe1a3b117506f
ELFUGUI25 <secocuellard@gmail.com>
2024-12-05 14:14:19 +0100

    PHP

--- PHP/PHP 4.php
+++ PHP/PHP 4.php
@@ -8,17 +8,9 @@
 </head>
 
 <body>
-    <h1>Combining HTML and PHP</h1>
-    <p>This is a static HTML paragraph.</p>
-
+    <h1>Combinacion</h1>
     <?php
-    // Aquí empieza el código PHP
-    echo "<p>This line is generated using PHP.</p>";
-
-    // Aquí puedes incluir cualquier lógica PHP adicional
-    $currentDate = date("Y-m-d");
-    echo "<p>Today's date is: $currentDate</p>";
-    // Aquí termina el código PHP
+    echo "<p>Esto es un buen ejemplazo </p>";
     ?>
 </body>
 
