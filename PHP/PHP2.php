5866ceb2fdf6cc762cf22417e2b0021202d4062e
ELFUGUI25 <secocuellard@gmail.com>
2024-12-05 13:58:26 +0100

    PHP

---
+++ PHP/PHP2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> EJRCICO 2</title>
+</head>
+
+<body>
+    <strong>¿Que significa php?</strong><br>
+    <echo>PHP se ejecuta en el servidor y nos devuelve la web en HTML. Además, es un lenguaje libre y muy popular con el
+        cual podemos crear cualquier web que deseemos, desde una web de información hasta una pasarela de pagos o una
+        tienda on-line. </echo>
+
+    <br><br><strong>¿Para que sirve echo?</strong><br>
+    <echo>Para escribir texto en pantalla, al igual que un "p" en html. </echo><br><br>
+
+    <strong>¿Que ocurre cuando ejecutas un archivo en el servidor?</strong><br>
+    <echo>Depende de algunos factores, pero puede generar una interpretacion de archivo, permisos de ejecucion, etc
+    </echo>
+
+</body>
+
+</html>
