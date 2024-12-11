b5208d372c33b79515cebc068e4388ef7fc01b2e
ELFUGUI25 <secocuellard@gmail.com>
2024-12-11 13:49:47 +0100

    PHP 2

--- PHP/PHP2.1.php
+++ PHP/PHP2.1.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP 1</title>
 </head>
+
 <body>
-    <?p
+    <?php
     echo "<p> Hola, ya se imprimir frases </p>"
-    ?>
+        ?>
 </body>
+
 </html>
