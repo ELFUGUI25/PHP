70021dae371ff6183785c48c2cbe9bcffb9db197
ELFUGUI25 <secocuellard@gmail.com>
2024-12-11 16:55:11 +0100

    PHP 2

---
+++ PHP/PHP 2.7.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 7</title>
+</head>
+
+<body>
+    <?php
+    $numero1 = 1234;
+    $numero2 = 532;
+
+    $multi = $numero1 * $numero2;
+
+    echo "El número es: $multi";
+    ?>
+</body>
+
+</html>
