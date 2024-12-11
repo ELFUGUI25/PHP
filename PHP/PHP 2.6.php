66907a58059fcc43edd97aba77ea95b9d6597792
ELFUGUI25 <secocuellard@gmail.com>
2024-12-11 16:39:09 +0100

    PHP 2

---
+++ PHP/PHP 2.6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 6</title>
+</head>
+
+<body>
+    dy>
+    <?php
+    $numero1 = 169;
+    $numero2 = 100;
+
+    $resta = $numero1 - $numero2;
+
+    echo "El número decimal es: $resta";
+    ?>
+</body>
+
+</html>
