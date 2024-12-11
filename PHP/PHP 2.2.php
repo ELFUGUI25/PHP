440b662860c8b44aa6351e3f3966644f250ce9e9
ELFUGUI25 <secocuellard@gmail.com>
2024-12-11 13:57:50 +0100

    PHP 2

---
+++ PHP/PHP 2.2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 2</title>
+</head>
+
+<body>
+    <?php
+    $numero = 025;
+    echo "El numero es: " . $numero;
+
+    ?>
+</body>
+
+</html>
