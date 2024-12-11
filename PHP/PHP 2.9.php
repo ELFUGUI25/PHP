b0ba346de0119eb439b74fce81693e82163ab78f
ELFUGUI25 <secocuellard@gmail.com>
2024-12-11 17:56:09 +0100

    PHP 2

---
+++ PHP/PHP 2.9.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 9</title>
+</head>
+
+<body>
+    <?php
+    $cadena = "Hola, este es un mensaje de ejemplo.";
+
+    $numero = 42;
+
+    echo "Mensaje: $cadena\n";
+    echo "Número: $numero\n";
+    ?>
+</body>
+
+</html>
