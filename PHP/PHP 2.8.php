7203ac2a7cdf99860f892faa8252913cbe0d797f
ELFUGUI25 <secocuellard@gmail.com>
2024-12-11 16:58:34 +0100

    PHP 2

---
+++ PHP/PHP 2.8.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 8</title>
+</head>
+
+<body>
+    <?php
+    $numero1 = 1234;
+    $numero2 = 532;
+
+    $div = $numero1 / $numero2;
+
+    echo "El número es: $div";
+    ?>
+</body>
+
+</html>
