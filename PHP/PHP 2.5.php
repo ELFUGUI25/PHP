4c9589eeba16868a1b3412561430951bcc4aca2b
ELFUGUI25 <secocuellard@gmail.com>
2024-12-11 16:28:17 +0100

    PHP 2

---
+++ PHP/PHP 2.5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 5</title>
+</head>
+
+<body>
+    <?php
+    $numero1 = 200;
+    $numero2 = 2235;
+
+    $suma = $numero1 + $numero2;
+
+    echo "El número decimal es: $suma";
+    ?>
+</body>
+
+</html>
