338d352f9f4b10fea62c0dfe7efd4b38d2b67ecc
ELFUGUI25 <secocuellard@gmail.com>
2024-12-12 16:34:25 +0100

    PHP 2

---
+++ PHP/PHP 2.16.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 16</title>
+</head>
+
+<body>
+    <?php
+    echo "Te voy a pedir el nombre para saludarte";
+    $nombre = David;
+
+    echo "Me llamo $nombre  <br>";
+    echo "Hola $nombre ";
+    ?>
+</body>
+
+</html>
