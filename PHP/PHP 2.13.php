de664e8be305428eb9eb8f938baa10b4f27a31c4
ELFUGUI25 <secocuellard@gmail.com>
2024-12-12 13:45:48 +0100

    PHP 2

---
+++ PHP/PHP 2.13.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 13</title>
+</head>
+
+<body>
+    <?php
+    $real1 = 10.5;
+    $real2 = 20.7;
+
+    $suma = $real1 + $real2;
+
+    echo "La suma de $real1 y $real2 es: $suma";
+    ?>
+
+</body>
+
+</html>
