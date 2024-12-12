2a224c57785c0feb94d4f9d9f977ba2e9e25baa4
ELFUGUI25 <secocuellard@gmail.com>
2024-12-12 14:08:11 +0100

    PHP 2

---
+++ PHP/PHP 2.15.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 15</title>
+</head>
+
+<body>
+    <?php
+    $kilobytes = 3600;
+    $megabytes = $kilobytes * 0.001;
+    $terabytes = $megabytes * 0.0000001;
+
+    echo "Equivalente en terabytes: $terabytes teras <br>";
+    echo "Equivalente en megabytes: $minutos megas";
+    ?>
+</body>
+
+</html>
