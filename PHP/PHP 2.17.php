381060502dae3edec82c10a39c1a1d4797951c10
ELFUGUI25 <secocuellard@gmail.com>
2024-12-12 16:49:47 +0100

    PHP 2

---
+++ PHP/PHP 2.17.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 17</title>
+</head>
+
+<body>
+    <?php
+
+    echo "Vamos a pasar de grados a Fahrenheit <br>";
+    $grados = 25;
+    $fahren = ($grados * 1.8) + 32;
+
+    echo "Si hace $grados ºC entonces son $fahren ºF <br>";
+    ?>
+</body>
+
+</html>
