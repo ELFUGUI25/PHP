474ad6ab67607e5fd3f7d9748c6e3315c19cb326
ELFUGUI25 <secocuellard@gmail.com>
2024-12-12 13:32:16 +0100

    PHP 2

---
+++ PHP/PHP 2.11.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 11</title>
+</head>
+
+<body>
+    <?php
+    $variable1 = "Nota:";
+    $variable2 = "Nota:";
+    $variable3 = "Nota:";
+    $media = ($variable1 + $variable2 + $variable3) / 3;
+
+    echo "La nota es: $variable1";
+    echo "La nota es: $variable2";
+    echo "La nota es: $variable3";
+
+    echo "La media de las notas es: $media";
+    ?>
+</body>
+
+</html>
