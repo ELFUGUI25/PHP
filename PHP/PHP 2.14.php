b9fac7dc2fa0b60f11b50e161bccb016eb6dda53
ELFUGUI25 <secocuellard@gmail.com>
2024-12-12 13:48:34 +0100

    PHP 2

---
+++ PHP/PHP 2.14.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 14</title>
+</head>
+
+<body>
+    <?php
+    $num1 = 10.5;
+    $num2 = 20.7;
+
+    $suma = $num1 + $num2;
+
+    echo "La suma de $num1 y $num2 es: $suma";
+    ?>
+</body>
+
+</html>
