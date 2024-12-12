55a02ca37c6fd7cd2b1ba94be0bc0d28f6aee869
ELFUGUI25 <secocuellard@gmail.com>
2024-12-12 13:40:30 +0100

    PHP 2

---
+++ PHP/PHP 2.12.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 12</title>
+</head>
+
+<body>
+    <?php
+    $real = 10.5;
+    $cadena = "Hola, este es un ejemplo.";
+
+    echo "El valor de la variable real es: $real\n";
+    echo "El valor de la variable cadena es: $cadena\n";
+    ?>
+</body>
+
+</html>
