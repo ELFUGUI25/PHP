3634a822def01c823af08cbde996a4f73a4bc7d5
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 10:31:57 +0100

    PHP 2

---
+++ PHP/PHP 2.21.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 21</title>
+</head>
+
+<body>
+    <?php
+    $base = 5;
+    $altura = 5;
+    $area = ($base * $altura) / 2;
+
+    echo "Vamos a calcular el area de un triangulo <br>";
+    echo "Si la base del triangulo es $base cm y la altura es $altura cm, el total serian= $area cm";
+    ?>
+</body>
+
+</html>
