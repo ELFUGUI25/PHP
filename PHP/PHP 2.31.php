991041dff97cb03309d0ad271d82a633dab32eef
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 13:45:06 +0100

    PHP 2

--- PHP/PHP 2.31.php
+++ PHP/PHP 2.31.php
@@ -10,19 +10,30 @@
 <body>
     <?php
 
-    $tabla2 = 2;
+    $tabla = 0 * 2;
+    $tabla1 = 1 * 2;
+    $tabla2 = 2 * 2;
+    $tabla3 = 3 * 2;
+    $tabla4 = 4 * 2;
+    $tabla5 = 5 * 2;
+    $tabla6 = 6 * 2;
+    $tabla7 = 7 * 2;
+    $tabla8 = 8 * 2;
+    $tabla9 = 9 * 2;
+    $tabla10 = 10 * 2;
 
-    echo "0 x 2 es: 0 * $tabla2 <br>";
-    echo "1 x 2 es: 1 * $tabla2<br>";
-    echo "2 x 2 es: 2 * $tabla2<br>";
-    echo "3 x 2 es: 3 * $tabla2 <br>";
-    echo "4 x 2 es: 4 * $tabla2<br>";
-    echo "5 x 2 es: 5 * $tabla2<br>";
-    echo "6 x 2 es: 6 * $tabla2<br>";
-    echo "7 x 2 es: 7 * $tabla2<br>";
-    echo "8 x 2 es: 8 * $tabla2<br>";
-    echo "9 x 2 es: 9 * $tabla2<br>";
-    echo "10 x 2 es: 10 * $tabla2<br>";
+    echo "ESTA ES LA TABLA DEL 2<br><br>";
+    echo "0 x 2 es: $tabla<br>";
+    echo "1 x 2 es: $tabla1<br>";
+    echo "2 x 2 es: $tabla2<br>";
+    echo "3 x 2 es: $tabla3<br>";
+    echo "4 x 2 es: $tabla4<br>";
+    echo "5 x 2 es: $tabla5<br>";
+    echo "6 x 2 es: $tabla6<br>";
+    echo "7 x 2 es: $tabla7<br>";
+    echo "8 x 2 es: $tabla8<br>";
+    echo "9 x 2 es: $tabla9<br>";
+    echo "10 x 2 es: $tabla10<br>";
 
     ?>
 </body>
