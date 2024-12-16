2fc0542e8250322a8862e9c8c7f0d5044554c638
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 10:24:48 +0100

    PHP 2

---
+++ PHP/PHP 2.19.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 19</title>
+</head>
+
+<body>
+    <?php
+    $lado = 5;
+    $area = $lado * $lado;
+
+    echo "Vamos a calcular el area de un cuadrado: <br>";
+    echo "Si el area del cuadrado es $lado cm, el total serian= $total cm";
+    ?>
+</body>
+
+</html>
