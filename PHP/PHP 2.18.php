30b27a9968eb693db2238208c7b470e072c9d0b3
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 10:18:58 +0100

    PHP 2

---
+++ PHP/PHP 2.18.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 18</title>
+</head>
+
+<body>
+    <?php
+    $horas = 20;
+    $sueldo = 9;
+    $sueldohora = $horas * $sueldo;
+
+    echo "Vamos a calcular el sueldo del fin de semana de una persona que ha trabajado ciertas horas:";
+    echo "Esta persona ha trabajado $horas horas";
+    echo "Si le pagan 9 euros la hora, el sueldo final es= $sueldohora";
+    ?>
+</body>
+
+</html>
