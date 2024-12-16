f67d4183a16a5021a20bdadb391092d75714af67
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 16:36:03 +0100

    PHP 2

--- PHP/PHP 2.35.php
+++ PHP/PHP 2.35.php
@@ -11,15 +11,16 @@
     <?php
 
     $comportamiento = 0.20;
-    $deberes = 0.50;
-    $examen = 0.30;
+    $deberes = 0.30;
+    $examen = 0.50;
 
     $nota1 = 7.5;
     $nota2 = 8;
     $nota3 = 6;
 
     $notaFinal = ($nota1 * $comportamiento) + ($nota2 * $deberes) + ($nota3 * $examen);
-
+    echo "El comportamiento cuenta un 20%, las tareas un 30% y el examen un 50%<br>";
+    echo "El alumno tiene un 7.5 en actitud, un 8 en las tareas realizadas y un 6 en los examenes<br>";
     echo "La nota final del alumno es: $notaFinal";
 
     ?>
