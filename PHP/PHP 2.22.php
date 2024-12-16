a26bdbf4a5213e7835d0996de8d2e1699c3dc182
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 10:36:27 +0100

    PHP 2

---
+++ PHP/PHP 2.22.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 22</title>
+</head>
+
+<body>
+    <?php
+    $radio = 5;
+    $pi = 3.1416;
+    $longitud = 2 * $pi * $radio;
+    $area = ($radio * 2) * $pi;
+
+    echo "Vamos a calcular el area de un circulo <br>";
+    echo "Si el radio del circulo es $radio cm, el total serian= $area cm";
+    ?>
+</body>
+
+</html>
