4a419e65209656ae96b72dfed1c5e24c02afcee3
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 13:40:55 +0100

    PHP 2

---
+++ PHP/PHP 2.31.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 31</title>
+</head>
+
+<body>
+    <?php
+
+    $tabla2 = 2;
+
+    echo "0 x 2 es: 0 * $tabla2 <br>";
+    echo "1 x 2 es: 1 * $tabla2<br>";
+    echo "2 x 2 es: 2 * $tabla2<br>";
+    echo "3 x 2 es: 3 * $tabla2 <br>";
+    echo "4 x 2 es: 4 * $tabla2<br>";
+    echo "5 x 2 es: 5 * $tabla2<br>";
+    echo "6 x 2 es: 6 * $tabla2<br>";
+    echo "7 x 2 es: 7 * $tabla2<br>";
+    echo "8 x 2 es: 8 * $tabla2<br>";
+    echo "9 x 2 es: 9 * $tabla2<br>";
+    echo "10 x 2 es: 10 * $tabla2<br>";
+
+    ?>
+</body>
+
+</html>
