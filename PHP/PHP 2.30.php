2cb6b7a8d029f0bec6327c2ead569e0195404a87
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 13:35:10 +0100

    PHP 2

--- PHP/PHP 2.30.php
+++ PHP/PHP 2.30.php
@@ -10,20 +10,21 @@
 <body>
     <?php
 
-    $sueldo = 10;
+    $sueldo1 = 10;
+
     $horas1 = 35;
     $horas2 = 42;
 
-    $calc1 = $compra1 - $descuento1;
-    $calc2 = $compra2 - $descuento2;
-    $calc3 = $compra3 - $descuento3;
+    $cobro1 = 10 * $horas1;
+    $cobro2 = (40 * $sueldo1) + (($horas2 - 40) * $sueldo1 * 1.5);
 
     echo " A un trabajador le pagan según sus horas trabajadas y la tarifa está a un valor por hora. Si la
 cantidad de horas trabajadas es mayor a 40 horas, la tarifa por hora se incrementa en un 50%
-para las horas extras. Calcular el salario del trabajador dadas las horas trabajadas y la tarifa<br><br>";
+para las horas extras.<br> Calcular el salario del trabajador dadas las horas trabajadas y la tarifa<br><br>";
 
-    echo "La primera semana el trabajador hecho 35 horas, la segunda semana hecho 42 horas, este trabajador cobra 10 euros la hora<br>";
-
+    echo "La primera semana el trabajador hecho 35 horas, la segunda semana hecho 42 horas, este trabajador cobra 10 euros la hora<br><br>";
+    echo "La primera semana trabajo $horas1 horas, por lo que le pagan= $cobro1 <br>";
+    echo "La segunda semana trabajo $horas2 horas, por lo que le pagan= $cobro2";
     ?>
 </body>
 
