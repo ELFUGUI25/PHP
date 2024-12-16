a4a2a546d446053fb22686d88ca3e3de8ea9e1fd
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 13:21:30 +0100

    PHP 2

---
+++ PHP/PHP 2.30.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 30</title>
+</head>
+
+<body>
+    <?php
+
+    $sueldo = 10;
+    $horas1 = 35;
+    $horas2 = 42;
+
+    $calc1 = $compra1 - $descuento1;
+    $calc2 = $compra2 - $descuento2;
+    $calc3 = $compra3 - $descuento3;
+
+    echo " A un trabajador le pagan según sus horas trabajadas y la tarifa está a un valor por hora. Si la
+cantidad de horas trabajadas es mayor a 40 horas, la tarifa por hora se incrementa en un 50%
+para las horas extras. Calcular el salario del trabajador dadas las horas trabajadas y la tarifa<br><br>";
+
+    echo "La primera semana el trabajador hecho 35 horas, la segunda semana hecho 42 horas, este trabajador cobra 10 euros la hora<br>";
+
+    ?>
+</body>
+
+</html>
