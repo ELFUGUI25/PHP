795a0be807ca5555bbe207c03e6c1ce4b6c9fe4d
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 16:26:27 +0100

    PHP 2

---
+++ PHP/PHP 2.34.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 34</title>
+</head>
+
+<body>
+    <?php
+
+    $v = 90;
+    $t = 1;
+    $d = $v * $t;
+
+    echo "Voy a calcular la la distancia que recorre un coche a $v km/h en $t hora<br><br>";
+
+    echo "Hago el calculo con la formula de la distancia que es d= v * t<br>";
+    echo "El resultado seria que ha recorrido $d metros en $t hora";
+
+    ?>
+</body>
+
+</html>
