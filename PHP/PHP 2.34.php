5cbee77c58d6f9bc395c0df2aa017e46975f9fed
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 16:29:19 +0100

    PHP 2

--- PHP/PHP 2.34.php
+++ PHP/PHP 2.34.php
@@ -12,12 +12,17 @@
 
     $v = 90;
     $t = 1;
+    $min = $t * 60;
+    $seg = $min * 60;
     $d = $v * $t;
+
 
     echo "Voy a calcular la la distancia que recorre un coche a $v km/h en $t hora<br><br>";
 
     echo "Hago el calculo con la formula de la distancia que es d= v * t<br>";
-    echo "El resultado seria que ha recorrido $d metros en $t hora";
+    echo "El resultado seria que ha recorrido $d km en $t hora<br>";
+    echo "Tambien podriamos decir que ha recorrido $d km en $min min o en $seg segundos ";
+
 
     ?>
 </body>
