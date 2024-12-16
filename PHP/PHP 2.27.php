6520d83e0b96a7f244947384a24bcd74d3586125
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 13:01:51 +0100

    PHP 2

---
+++ PHP/PHP 2.27.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 27</title>
+</head>
+
+<body>
+    <?php
+    $num1 = 10;
+    $num2 = 10;
+
+    $suma = $num1 + $num2;
+    $resta = $num1 - $num2;
+    $multiplicacion = $num1 * $num2;
+    $division = $num1 / $num2;
+
+
+    echo "Vamos a calcular la suma, resta, multiplicacion y division de dos numeros <br><br>";
+
+    echo "La suma es: $suma  <br>";
+    echo "La resta es: $resta <br>";
+    echo "La multiplicacion es: $multiplicacion <br>";
+    echo "La division es: $division <br>";
+    ?>
+</body>
+
+</html>
