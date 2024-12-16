6d61927419768850e0b6555f9fcbea76cbdddce4
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 13:57:04 +0100

    PHP 2

---
+++ PHP/PHP 2.33.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 33</title>
+</head>
+
+<body>
+    <?php
+
+    $c1 = 10;
+    $c1 = 7;
+
+
+    echo "Voy a calcular la hipotenusa de un triangulo<br><br>";
+
+    echo "La suma de los catetos al cuadrado es igual a la hipotenusa:";
+    echo "La hpitenusa es: " . pow($c1, 2) + pow($c2, 2) . "<br>";
+
+    ?>
+</body>
+
+</html>
