fe64c45b820906fdc911a65c3590e083e78b81b9
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 13:48:32 +0100

    PHP 2

---
+++ PHP/PHP 2.32.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 32</title>
+</head>
+
+<body>
+    <?php
+
+    echo "ESTO SON LOS NUMERO DEL 1 AL 9<br><br>";
+    echo "Aqui va el 1<br>";
+    echo "Aqui va el 2<br>";
+    echo "Aqui va el 3<br>";
+    echo "Aqui va el 4<br>";
+    echo "Aqui va el 5<br>";
+    echo "Aqui va el 6<br>";
+    echo "Aqui va el 7<br>";
+    echo "Aqui va el 8<br>";
+    echo "Aqui va el 9<br>";
+    ?>
+</body>
+
+</html>
