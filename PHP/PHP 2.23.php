481905db78ff22c228de5496dd9ce0c52728f30f
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 10:42:55 +0100

    PHP 2

---
+++ PHP/PHP 2.23.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 23</title>
+</head>
+
+<body> <?php
+$descuento = 0.1;
+$viagra = 5;
+$desc_viagra = 5 * 0.1;
+$proteccion = 10;
+$desc_protecc = 10 * 0.1;
+
+echo "Se va a hacer un descuento en la farmacia <br>";
+echo "Los productos seran rebajados un 10% <br>";
+echo "Las pastillas para la ereccion costaban 5 euros, ahora cuestan: $desc_viagra <br>";
+echo "La proteccion para el gusanillo costaban 10 euros, ahora cuestan: $desc_protecc <br>";
+?>
+</body>
+
+</html>
