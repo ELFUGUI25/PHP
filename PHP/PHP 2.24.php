9f61b350813b7c8f165eee35f98e5fa7228079db
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 12:29:57 +0100

    PHP 2

---
+++ PHP/PHP 2.24.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 24</title>
+</head>
+
+<body>
+    <?php
+    $niños = 17;
+    $niñes = 10;
+
+    $total = $niños + $niñes;
+
+    $porc_niño = $niños / $total;
+    $porc_niñe = $niñas / $total;
+
+    echo "Esta es la clase de 2 año de ASIR";
+    echo "En esta clase hay $niños niños y $niñes niñes <br>";
+    echo "El porcentaje de cada niños de esta clase es, $porc_niño % <br>";
+    echo "El porcentaje de cada niñes de esta clase es, $porc_niñe % <br>";
+    ?>
+
+</body>
+
+</html>
