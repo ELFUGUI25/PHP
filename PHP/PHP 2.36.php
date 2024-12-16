9e5370eb00101985d21b9299d3bf3468a659e7f0
ELFUGUI25 <secocuellard@gmail.com>
2024-12-16 16:43:06 +0100

    PHP 2

---
+++ PHP/PHP 2.36.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 36</title>
+</head>
+
+<body>
+    <?php
+
+    $num1 = 1;
+    $num2 = 2;
+    $num3 = 3;
+    $num4 = 4;
+    $num5 = 5;
+    $num6 = 6;
+    $num7 = 7;
+    $num8 = 8;
+    $num9 = 9;
+
+    $cuad1 = $num1 * $num1;
+    $cuad2 = $num2 * $num2;
+    $cuad3 = $num3 * $num3;
+    $cuad4 = $num4 * $num4;
+    $cuad5 = $num5 * $num5;
+    $cuad6 = $num6 * $num6;
+    $cuad7 = $num7 * $num7;
+    $cuad8 = $num8 * $num8;
+    $cuad9 = $num9 * $num9;
+
+    echo "ESTO SON LOS CUADRADOS DEL 1 AL 9<br><br>";
+    echo "El cuadrado de $num1 es $cuad1";
+    echo "El cuadrado de $num2 es $cuad2";
+    echo "El cuadrado de $num3 es $cuad4";
+    echo "El cuadrado de $num4 es $cuad4";
+    echo "El cuadrado de $num5 es $cuad5";
+    echo "El cuadrado de $num6 es $cuad6";
+    echo "El cuadrado de $num7 es $cuad7";
+    echo "El cuadrado de $num8 es $cuad8";
+    echo "El cuadrado de $num9 es $cuad4";
+    ?>
+</body>
+
+</html>
