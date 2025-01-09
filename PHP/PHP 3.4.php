b2be38556ba4bd5e73083fe681bc5b6b28e6f3b3
dsecoc01 <secocuellard@gmail.com>
2025-01-09 14:01:40 +0100

    PHP 3.4

---
+++ PHP/PHP 3.4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 3.1</title>
+</head>
+<body>
+    <?php
+      $var = 5
+      echo "se van a escribir numeros del 5 al 10<br>";
+      while ($var <= 10){
+    
+       echo "$var<br>";
+       $var++;
+    }
+    ?>
+</body>
+</html>
