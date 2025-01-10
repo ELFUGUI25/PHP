026e015320c576f62aa4b803bc72ec36e28c8c6e
dsecoc01 <secocuellard@gmail.com>
2025-01-10 08:21:37 +0100

    PHP 3.7

---
+++ PHP/PHP 3.7.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 3.1</title>
+</head>
+<body>
+    <?php
+      $var = 1;
+      echo "se va a escribir hola 200 veces<br>";
+      while ($var <= 200){
+    
+       echo "hola<br>";
+       $var++;
+    }
+    ?>
+</body>
+</html>
