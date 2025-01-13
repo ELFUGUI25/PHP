a4554bcb35e06bfb611d9714fe5bd28cf3dc4419
dsecoc01 <secocuellard@gmail.com>
2025-01-13 16:46:04 +0100

    PHP 3.8

---
+++ PHP/PHP 3.11.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3.11</title>
+</head>
+<body>
+<?php
+
+$suma_cuadrados = 0;
+
+for ($i = 1; $i <= 100; $i++) {
+    $suma_cuadrados += $i * $i; // Cuadrado del número actual
+}
+
+echo "La suma de los cuadrados de los 100 primeros números naturales es: $suma_cuadrados\n";
+?>
+
+</body>
+</html>
