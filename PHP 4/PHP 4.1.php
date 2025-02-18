c5b7e9ebd53fb3ca8b43e47808066d806f29793e
dsecoc01 <secocuellard@gmail.com>
2025-02-18 13:33:11 +0100

    PHP 4.1

---
+++ PHP 4/PHP 4.1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 4.1</title>
+</head>
+<body>
+<?php
+$numeroAleatorio = rand(1, 100);
+echo $numeroAleatorio;
+?>
+
+</body>
+</html>
