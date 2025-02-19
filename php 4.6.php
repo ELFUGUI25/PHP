d7c72a4aa1d43350390701b598052b8c38299413
dsecoc01 <secocuellard@gmail.com>
2025-02-19 13:45:28 +0100

    php 4.6

---
+++ php 4.6.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php 4.6</title>
+</head>
+<body>
+<?php
+$variable = "Hola Mundo";
+
+if (isset($variable)) {
+    echo "La variable está definida y no es nulo.";
+} else {
+    echo "La variable no está definida o es nulo.";
+}
+?>
+
+</body>
+</html>
