37b306560ce7adb7fe162c6cdfb1ef92e9cc63e9
dsecoc01 <secocuellard@gmail.com>
2025-02-19 13:44:06 +0100

    php 4.5

---
+++ php 4.5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$variable = "";
+
+if (empty($variable)) {
+    echo "La variable está vacía.";
+} else {
+    echo "La variable no está vacía.";
+}
+?>
+
+</body>
+</html>
