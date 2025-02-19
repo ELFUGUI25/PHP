b9c8b4c243ac4b6984485415e4946c2c1f2aea5f
dsecoc01 <secocuellard@gmail.com>
2025-02-19 13:38:34 +0100

    php 4.3

---
+++ php 4.3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php 4.3</title>
+</head>
+<body>
+<?php
+$numeroDecimal = 15.78;
+$numeroEntero = intval($numeroDecimal);
+
+echo "El número entero es: " . $numeroEntero;
+?>
+
+</body>
+</html>
