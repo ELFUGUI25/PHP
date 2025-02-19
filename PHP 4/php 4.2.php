7e73464a91951f1ef3efc7062c72715fbf4a0525
dsecoc01 <secocuellard@gmail.com>
2025-02-19 13:35:04 +0100

    php 4.2

---
+++ PHP 4/php 4.2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php 4.2</title>
+</head>
+<body>
+<?php
+$min = 10; 
+$max = 50; 
+
+$numeroAleatorio = rand($min, $max);
+
+echo "El número aleatorio es: " . $numeroAleatorio;
+?>
+
+</body>
+</html>
